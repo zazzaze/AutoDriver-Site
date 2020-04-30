d4ed028795cfd6a2fc56895e1f61b581e46791e1
Egor <egor.anikeev.00@gmail.com>
2020-04-30 11:18:58 +0500

    создан проект

---
+++ Driver/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Driver.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
